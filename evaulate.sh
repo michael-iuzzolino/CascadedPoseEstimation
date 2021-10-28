c2e2db62b15a9ff701b29e2be83db9a0135c1349
michael-iuzzolino <michael.iuzzolino@colorado.edu>
2021-10-27 18:33:28 -0600

    update

--- evaulate.sh
+++ evaulate.sh
@@ -2,21 +2,15 @@
 
 CFG_ROOT="experiments/mpii/hourglass"
 cfg_list=(
-#   "${CFG_ROOT}/hourglass_4__td_1__distillation__alpha_0_75.yaml"
-#   "${CFG_ROOT}/hourglass_4__td_0_5__distillation__alpha_0_75.yaml"
-#   "${CFG_ROOT}/hourglass_4__td_1__distillation__alpha_0_25.yaml"
-#   "${CFG_ROOT}/hourglass_4__td_0_9__distillation__alpha_0.yaml"
-  "${CFG_ROOT}/hourglass_4__td_0_9__distillation__alpha_0_25.yaml"
-  "${CFG_ROOT}/hourglass_4__td_0_9__distillation__alpha_0_75.yaml"
-#   "${CFG_ROOT}/hourglass_4__td_0_5__distillation__alpha_0_25.yaml"
-#   "${CFG_ROOT}/hourglass_4__td_0__distillation__alpha_0.yaml"
-#   "${CFG_ROOT}/hourglass_4__td_0__distillation__alpha_1.yaml"
+# PUGET
+  "${CFG_ROOT}/hourglass_4__td_0_95__distillation__alpha_0_25.yaml"
 )
 for cfg in "${cfg_list[@]}"
 do
     cmd=( python pose_estimation/valid.py )   # create array with one element
     cmd+=( --cfg $cfg )
     cmd+=( --force_overwrite )
-    # Run command
+    cmd+=( --load_best_ckpt )
+#     Run command
     "${cmd[@]}"
 done
